d440a8bf55754e647d7ddf7d001e98927aa7fe3e
masudra <masudrana6278312@gmail.com>
2023-05-02 00:54:24 +0600

    fast commit

--- src/Pages/Sheird/LeftNav/LeftNav.jsx
+++ src/Pages/Sheird/LeftNav/LeftNav.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+    const [cateagoris, setCateagoris]=useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5003/cateagori')
+        .then(res => res.json())
+        .then(data => setCateagoris(data))
+
+    },[])
+
     return (
         <div>
             <h1>this is left header</h1>
+            <div>
+                {
+                  cateagoris.map(categori => <p key={categori.id}>
+
+                    <Link  className='text-decoration-none text-black' to={`/cateagori/${categori.id}`}>{categori.name}</Link>
+
+                  </p>)
+                }
+
+
+            </div>
             
         </div>
     );
